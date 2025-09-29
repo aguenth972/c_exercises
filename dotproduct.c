1f4063b643f2e2a582c0bce10e200b48c18a309f
Aiden Guenther <guenthea@whitman.edu>
2025-09-29 13:13:12 -0700

    First Vector Implementation

---
+++ dotproduct.c
@@ -0,0 +1,11 @@
+// Calculate the Dot Product between two vectors
+
+int main() {
+    int dotProduct = 0;
+    int firstVector[3], secondVector[3];
+    
+    printf("Enter 3 Components of 1st Vector");
+    scanf("%d %d %d", &firstVector[0], &firstVector[1], &firstVector[2]);
+    
+
+}
